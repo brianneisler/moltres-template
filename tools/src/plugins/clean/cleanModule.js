2701176de644a03759a1eb222dfb9614c6b3e7f7
Brian Neisler <brian@airbug.com>
2018-06-04 22:14:14 -0700

    Add removal of moltres folder to clean step

--- tools/src/plugins/clean/cleanModule.js
+++ tools/src/plugins/clean/cleanModule.js
@@ -5,8 +5,10 @@ const cleanModule = async (module, context) => {
   const { logger } = context
   logger.log(`cleaning module ${module.name}`)
   const moduleDist = resolve(module.path, 'dist')
+  const moduleMoltres = resolve(module.path, '.moltres')
   return execScripts([
-    `rm -rf ${moduleDist}`
+    `rm -rf ${moduleDist}`,
+    `rm -rf ${moduleMoltres}`
   ], {
     cwd: module.modulesDir,
     env: {
