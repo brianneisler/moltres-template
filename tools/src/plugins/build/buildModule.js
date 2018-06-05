126dbd6584232a49d3197182777fdfe979b31858
Brian Neisler <brian@airbug.com>
2018-06-04 20:34:12 -0700

    Fix npm packing for modules

--- tools/src/plugins/build/buildModule.js
+++ tools/src/plugins/build/buildModule.js
@@ -11,13 +11,24 @@ const buildModule = async (module, context) => {
   )
   const moduleSrc = resolve(module.path, 'src')
   const moduleDist = resolve(module.path, 'dist')
-  return execScripts([
+  const moduleMoltres = resolve(module.path, '.moltres')
+  await execScripts([
     `mkdir -p ${moduleDist}`,
+    `mkdir -p ${moduleMoltres}`,
     `${babel} ${moduleSrc} -d ${moduleDist} --source-maps --ignore **/*.test.js`,
-    `rsync -avz --exclude *.js --exclude __tests__ --exclude node_modules ${moduleSrc}/ ${moduleDist}/`,
-    `npm pack`
+    `rsync -avz --exclude *.js --exclude __tests__ --exclude node_modules ${moduleSrc}/ ${moduleDist}/`
   ], {
     cwd: module.modulesDir,
+    env:  {
+      ...process.env,
+      ...context.env,
+      MOLTRES_STAGE: context.stage
+    }
+  })
+  return execScripts([
+    `npm pack ${module.path}`
+  ], {
+    cwd: moduleMoltres,
     env:  {
       ...process.env,
       ...context.env,
