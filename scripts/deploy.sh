54db4695226a7b6c065d508c7296657fb3cd0aa8
Brian Neisler <me@brianneisler.com>
2018-06-29 22:16:52 -0700

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && -n "$TRAVIS_TAG" ]]; then
   if [[ "$TRAVIS_NODE_VERSION" == "8" ]]; then
     echo "Preparing to deploy project"
-    node ./scripts/deploy/exec.js
+    babel-node ./scripts/deploy/exec.js
   else
     echo "Skipping project deployment since it's not a node 8"
   fi
