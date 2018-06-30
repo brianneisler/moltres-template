2ab5b4104f1bcd3135d1f51d00dffd262643e972
Brian Neisler <me@brianneisler.com>
2018-06-29 21:25:49 -0700

    Prevent deploy on every build and only deploy using node 8. Don't upgrade npm.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-#if [[ "$TRAVIS_PULL_REQUEST" == "false" && -n "$TRAVIS_TAG" ]]; then
-  echo "Preparing to deploy project"
-  node ./scripts/deploy/exec.js
-#else
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && -n "$TRAVIS_TAG" ]]; then
+  if [[ "$TRAVIS_NODE_VERSION" == "8" ]]; then
+    echo "Preparing to deploy project"
+    node ./scripts/deploy/exec.js
+  else
+    echo "Skipping project deployment since it's not a node 8"
+  fi
+else
   echo "Skipping project deployment since it's not a tagged commit"
-#fi
+fi
--- scripts/setup-ci.sh
+++ scripts/setup-ci.sh
@@ -1,3 +1,3 @@
 # upgrade to latest npm to fix parallel install issues in npm 5
-npm install -g npm
+# npm install -g npm
 sudo apt-get install graphicsmagick
