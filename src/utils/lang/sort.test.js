6bdab04b928c1bd4a4c6605e410cf7e66d3e7269
Brian Neisler <me@brianneisler.com>
2020-07-08 07:10:44 -0700

    Fix sort test

--- src/utils/lang/sort.test.js
+++ src/utils/lang/sort.test.js
@@ -4,13 +4,29 @@ import sort from './sort'
 describe('sort', () => {
   test('sorts an array of strings in alphabetical order', () => {
     const values = ['foo', 'bar', 'baz']
-    const result = sort((itemA, itemB) => itemA > itemB, values)
+    const result = sort((itemA, itemB) => {
+      if (itemA > itemB) {
+        return 1
+      }
+      if (itemB > itemA) {
+        return -1
+      }
+      return 0
+    }, values)
     expect(result).toEqual(['bar', 'baz', 'foo'])
   })
 
   test('sorts an ImmutableList of strings in alphabetical order', () => {
     const values = ImmutableList(['foo', 'bar', 'baz'])
-    const result = sort((itemA, itemB) => itemA > itemB, values)
+    const result = sort((itemA, itemB) => {
+      if (itemA > itemB) {
+        return 1
+      }
+      if (itemB > itemA) {
+        return -1
+      }
+      return 0
+    }, values)
     expect(result).toEqual(ImmutableList(['bar', 'baz', 'foo']))
   })
 })
