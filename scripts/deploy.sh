738e6cc2c6660bb184abc3367feb917da672cd26
Brian Neisler <me@brianneisler.com>
2020-09-25 18:12:51 -0700

    Add deployment fixes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,20 @@ echo "deploying ${STAGE}..."
 npm run clean
 
 # Set the firebase config
-DOTENV=$(cat "./stages/${STAGE}/.env")
-firebase functions:config:set dot.env="$DOTENV" --project $FIREBASE_PROJECT_ID --token "$FIREBASE_TOKEN"
+# DOTENV=$(cat "./stages/${STAGE}/.env")
+# firebase functions:config:set dot.env="$DOTENV" --project $FIREBASE_PROJECT_ID --token "$FIREBASE_TOKEN"
+
+# set the firebase stage in functions config
+firebase functions:config:set functions.stage="$STAGE" --project $FIREBASE_PROJECT_ID --token "$FIREBASE_TOKEN"
 
 npm run build
 
 # Backup the database before deploy the changes
 npm run database:backup
+
+# NOTE BRN: We need to export this here so that google cloud functions can
+# access private npm modules
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
 
 echo "starting deploy for ${STAGE}..."
 firebase deploy --project $FIREBASE_PROJECT_ID --token "$FIREBASE_TOKEN"
--- src/functions/setupFunctionConfig.js
+++ src/functions/setupFunctionConfig.js
@@ -1,12 +1,18 @@
+import * as functions from 'firebase-functions'
+
 import { loadProjectConfigSync } from '../config'
 import * as coreModules from '../core/modules'
+import { getPath } from '../utils/lang'
+import { pathResolve } from '../utils/path'
 
 const setupFunctionConfig = (modules) => {
   return loadProjectConfigSync({
+    cwd: pathResolve(__dirname, '..', '..'),
     modules: {
       ...coreModules,
       ...modules
     },
+    stage: getPath(['functions', 'stage'], functions.config()),
     target: 'function'
   })
 }
