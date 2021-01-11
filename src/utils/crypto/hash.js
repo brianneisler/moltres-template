01896addf03f16883403b9214683674de7f56fa9
Brian Neisler <me@brianneisler.com>
2021-01-11 10:48:10 -0500

    Add md5 hash to crypto utils

--- src/utils/crypto/hash.js
+++ src/utils/crypto/hash.js
@@ -1,13 +1,20 @@
+import md5 from 'crypto-js/md5'
 import sha256 from 'crypto-js/sha256'
 
-import { SHA256 } from '../../constants/Algorithm'
+import { MD5, SHA256 } from '../../constants/Algorithm'
 import { curry } from '../lang'
 
+const HASH_FUNCTIONS = {
+  [MD5]: md5,
+  [SHA256]: sha256
+}
+
 const hash = curry((algorithm, value) => {
-  if (algorithm === SHA256) {
-    return sha256(value).toString()
+  const hashFunction = HASH_FUNCTIONS[algorithm]
+  if (!hashFunction) {
+    throw new Error(`unsupported algorithm ${algorithm}`)
   }
-  throw new Error(`unsupported algorithm ${algorithm}`)
+  hashFunction(value).toString()
 })
 
 export default hash
---
+++ src/utils/crypto/hashMd5.js
@@ -0,0 +1,7 @@
+import { MD5 } from '../../constants/Algorithm'
+
+import hash from './hash'
+
+const hashMd5 = hash(MD5)
+
+export default hashMd5
